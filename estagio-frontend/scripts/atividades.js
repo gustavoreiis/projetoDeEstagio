0d8673211bcaea320a040a3c90211a99bbc1e3b9
SOAM\2716629 <2716629@tcs.com>
2025-09-18 17:24:25 -0300

    Atividades

--- estagio-frontend/scripts/atividades.js
+++ estagio-frontend/scripts/atividades.js
@@ -2,3 +2,14 @@ async function abrirModalCriar () {
     const modalCriar = new bootstrap.Modal(document.getElementById('modalCriarAtividade'));
     modalCriar.show();
 }
+
+const collapse = document.getElementById('pesquisaCollapse');
+const icone = document.getElementById('iconeCollapse');
+collapse.addEventListener('shown.bs.collapse', () => {
+    icone.classList.remove('bi-caret-down-fill');
+    icone.classList.add('bi-caret-up-fill');
+});
+collapse.addEventListener('hidden.bs.collapse', () => {
+    icone.classList.remove('bi-caret-up-fill');
+    icone.classList.add('bi-caret-down-fill');
+});
